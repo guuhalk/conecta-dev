575be4cb608b7e276a248b86754ff84427d2ae4d
Gustavo Daniel <gustavodaniel.santos@outlook.com>
2021-04-03 12:17:25 -0300

    inicio do projeto

---
+++ src/pages/Home/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Header(){
+    return (
+        <header className="header">
+            <div className="toolbar">
+                <div className="" >
+                    <span>Connecta Dev</span>
+                </div>
+                    
+                <div className="" >
+                    <button>NovoPost</button>
+                    <span>img1</span>
+                    <span>img2</span>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './home.css'
+import Header from './components/Header'
+
+function Home() {
+    return (
+        <div>
+            <Header />
+            
+            <main className="main" >
+                <div className="navbar">
+                    navbarR
+                </div>
+                <div className="feed">
+                    feed
+                </div>
+            </main>
+        </div>   
+    );
+    
+    
+
+}  
+
+export default Home;
